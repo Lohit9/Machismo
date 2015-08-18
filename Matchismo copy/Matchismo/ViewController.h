27fec21235501092532db4064e7770541c3465de
Lohit9 <tlohit@hotmail.com>
2015-08-18 01:11:20 -0400

    added first file

---
+++ Matchismo copy/Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Lohit Talasila on 2015-08-18.
+//  Copyright © 2015 Lohit Talasila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo copy/Matchismo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Lohit Talasila on 2015-08-18.
+//  Copyright © 2015 Lohit Talasila. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
